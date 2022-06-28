7647df0b37ffee2acc48f8ad82284690c0176fb6
Your Name <you@example.com>
2022-06-28 14:54:26 +0200

    map fixen

--- Wow.java
+++ Wow.java
@@ -8,7 +8,7 @@ public class Wow {
 
   private static void printMap(MapPosition[][] map) {
     for (int y = 8; y >= 0; y--) {
-      for (int x = 0; x < 8; x++) {
+      for (int x = 0; x <= 8; x++) {
         System.out.print(map[x][y].getElement().getSymbol());         
       }
       System.out.println();
@@ -17,20 +17,21 @@ public class Wow {
 
   private static MapPosition[][] createMap() {
 
-    MapPosition[][] map = new MapPosition[8][9];
-    map[0][0] = new MapPosition(new Field("Dein Zuhause."));
+    MapPosition[][] map = new MapPosition[9][9];
+    
 
     for (int y = 0; y <= 8; y++) {
-      for (int x = 0; x < 8; x++) {
-        map[x][y] = new MapPosition(new Field("Hier ist Nichts."));
+      for (int x = 0; x <= 8; x++) {
+        map[x][y] = new MapPosition(new Field("HALLO HIER IST DAS MEER WILLST DU SCHWIMMEN ODER WAS?!!"));
       }
     }
 
     for (int n = 0; n < 8; n++) {
-      map[7][n] = new MapPosition(new Field("Hier ist das Meer."));
+      map[8][n] = new MapPosition(new Field("Hier ist das Meer."));
       map[n][8] = new MapPosition(new Field("Hier ist das Meer."));
     }
 
+    map[0][0] = new MapPosition(new Field("Hier ist dein Zuhause."));
     map[0][1] = new MapPosition(new Meer());
     map[0][2] = new MapPosition(new Meer());
     map[0][3] = new MapPosition(new Meer());
@@ -40,6 +41,7 @@ public class Wow {
     map[0][7] = new MapPosition(new Meer());
     map[0][8] = new MapPosition(new Meer());
 
+
     map[1][0] = new MapPosition(new Field("Hier ist der Waldweg."));
     map[1][1] = new MapPosition(new Field("Hier ist der Waldweg."));
     map[1][2] = new MapPosition(new Wald());
@@ -48,17 +50,17 @@ public class Wow {
     map[1][5] = new MapPosition(new Wald());
     map[1][6] = new MapPosition(new Wald());
     map[1][7] = new MapPosition(new Wald());
-    map[1][8] = new MapPosition(new Wald());
+    map[1][8] = new MapPosition(new Meer());
 
     map[2][0] = new MapPosition(new Meer());
     map[2][1] = new MapPosition(new Field("Hier ist der Waldweg."));
     map[2][2] = new MapPosition(new Field("Hier ist der Waldweg."));
-    map[2][3] = new MapPosition(new Field("Der Marktplatz"));
+    map[2][3] = new MapPosition(new Field("Hier ist der Waldweg."));
     map[2][4] = new MapPosition(new Field("Hier ist der Markt."));
     map[2][5] = new MapPosition(new Wald());
     map[2][6] = new MapPosition(new Wald());
     map[2][7] = new MapPosition(new Wald());
-    map[2][8] = new MapPosition(new Wald());
+    map[2][8] = new MapPosition(new Meer());
 
     map[3][0] = new MapPosition(new Meer());
     map[3][1] = new MapPosition(new Wald());
@@ -68,31 +70,31 @@ public class Wow {
     map[3][5] = new MapPosition(new Wald());
     map[3][6] = new MapPosition(new Wald());
     map[3][7] = new MapPosition(new Wald());
-    map[3][8] = new MapPosition(new Wald());
+    map[3][8] = new MapPosition(new Meer());
 
     map[4][0] = new MapPosition(new Meer());
     map[4][1] = new MapPosition(new Wald());
     map[4][2] = new MapPosition(new Wald());
     map[4][3] = new MapPosition(new Wald());
-    map[4][4] = new MapPosition(new Field("Hier ist der Waldweg."));
+    map[4][4] = new MapPosition(new Wald());
     map[4][5] = new MapPosition(new Wald());
     map[4][6] = new MapPosition(new Wald());
     map[4][7] = new MapPosition(new Wald());
-    map[4][8] = new MapPosition(new Wald());
+    map[4][8] = new MapPosition(new Meer());
 
     map[5][0] = new MapPosition(new Meer());
-    map[5][1] = new MapPosition(new Field("Missionsgebiet: Die Koboldshöhle."));
-    map[5][2] = new MapPosition(new Field("Missionsgebiet: Die Koboldshöhle."));
-    map[5][3] = new MapPosition(new Field("Hier ist der Waldweg."));
-    map[5][4] = new MapPosition(new Field("Hier ist der Waldweg."));
+    map[5][1] = new MapPosition(new Koboldcave());
+    map[5][2] = new MapPosition(new Koboldcave());
+    map[5][3] = new MapPosition(new Wald());
+    map[5][4] = new MapPosition(new Wald());
     map[5][5] = new MapPosition(new Wald());
     map[5][6] = new MapPosition(new Ruinen());
     map[5][7] = new MapPosition(new Ruinen());
     map[5][8] = new MapPosition(new Meer());
 
     map[6][0] = new MapPosition(new Meer());
-    map[6][1] = new MapPosition(new Field("Missionsgebiet: Die Koboldshöhle."));
-    map[6][2] = new MapPosition(new Field("Missionsgebiet: Die Koboldshöhle."));
+    map[6][1] = new MapPosition(new Koboldcave());
+    map[6][2] = new MapPosition(new Koboldcave());
     map[6][3] = new MapPosition(new Wald());
     map[6][4] = new MapPosition(new Wald());
     map[6][5] = new MapPosition(new Wald());
@@ -101,14 +103,24 @@ public class Wow {
     map[6][8] = new MapPosition(new Meer());
 
     map[7][0] = new MapPosition(new Meer());
-    map[7][1] = new MapPosition(new Meer());
-    map[7][2] = new MapPosition(new Meer());
-    map[7][3] = new MapPosition(new Meer());
-    map[7][4] = new MapPosition(new Meer()); 
-    map[7][5] = new MapPosition(new Meer());
-    map[7][6] = new MapPosition(new Meer());
-    map[7][7] = new MapPosition(new Meer());
+    map[7][1] = new MapPosition(new Wald());
+    map[7][2] = new MapPosition(new Wald());
+    map[7][3] = new MapPosition(new Wald());
+    map[7][4] = new MapPosition(new Wald()); 
+    map[7][5] = new MapPosition(new Wald());
+    map[7][6] = new MapPosition(new Wald());
+    map[7][7] = new MapPosition(new Wald());
     map[7][8] = new MapPosition(new Meer());
+    
+    map[8][0] = new MapPosition(new Meer());
+    map[8][1] = new MapPosition(new Meer());
+    map[8][2] = new MapPosition(new Meer());
+    map[8][3] = new MapPosition(new Meer());
+    map[8][4] = new MapPosition(new Meer());
+    map[8][5] = new MapPosition(new Meer());
+    map[8][6] = new MapPosition(new Meer());
+    map[8][7] = new MapPosition(new Meer());
+
 
     return map;
   }
@@ -168,7 +180,7 @@ public class Wow {
           y--;
           duSpieler.MoveDown();
           printMap(map);
-          System.out.println("*Du hast einen Schritt in Richtung Süden gemacht*" + "Ich befinde mich jetzt bei P(" + x
+          System.out.println("*Du hast einen Schritt in Richtung Süden gemacht* " + "Ich befinde mich jetzt bei P(" + x
               + "|" + y + ").");
           System.out.println("Feld: " + map[x][y].getElement().getDisplayName());
           break;
@@ -176,7 +188,7 @@ public class Wow {
           x--;
           duSpieler.MoveLeft();
           printMap(map);
-          System.out.println("*Du hast einen Schritt in Richtung Westen gemacht*" + "Ich befinde mich jetzt bei P(" + x
+          System.out.println("*Du hast einen Schritt in Richtung Westen gemacht* " + "Ich befinde mich jetzt bei P(" + x
               + "|" + y + ").");
           System.out.println("Feld: " + map[x][y].getElement().getDisplayName());
           break;
