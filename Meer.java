2ec4a686826cbd624f22e7cf0d0ffa29af1709c1
Your Name <you@example.com>
2022-07-01 13:57:14 +0200

    CELANUP

--- Meer.java
+++ Meer.java
@@ -1,12 +1,8 @@
 public class Meer implements MapElement {
   public String getDisplayName() {
     return "Hier ist das Meer.";
-}
+  }
     public String getSymbol() {
         return "~";
-      }
-
-
-
-
+  }
 }
